d88edfc29a28f147975bf92baaf58959e994bd3a
Flamingfury56 <nwe4309@g.rit.edu>
2018-10-28 15:24:31 -0400

    Finished examine script

--- Assets/Scripts/Examine.cs
+++ Assets/Scripts/Examine.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Examine : MonoBehaviour
 {
@@ -8,7 +9,13 @@ public class Examine : MonoBehaviour
     public GameObject target;
     public GameObject player;
 
+    public GameObject examineTextUI;
+    public Sprite imageToShow;
+    public GameObject page;
+
     Vector3 targetPoint;
+
+    bool closeEnough;
 
 	// Use this for initialization
 	void Start ()
@@ -23,9 +30,29 @@ public class Examine : MonoBehaviour
         Vector3 playerPos = player.transform.position;
         playerPos.y = 0;
 
-		if(Vector3.Distance(playerPos,targetPoint) <= 2)
+		if(Vector3.Distance(playerPos,targetPoint) <= 1f)
         {
+            examineTextUI.SetActive(true);
+            closeEnough = true;
+
+            if (Input.GetKeyDown("e"))
+            {
+                page.GetComponent<Image>().sprite = imageToShow;
+                page.SetActive(true);
+            }
 
         }
-	}
+        else if(closeEnough)
+        {
+            examineTextUI.SetActive(false);
+            closeEnough = false;
+        }
+
+        if (page.activeSelf == true && (Input.GetKeyDown(KeyCode.Escape)))
+        {
+            page.SetActive(false);
+        }
+
+
+    }
 }
