71cc03ecc873d169294935c3bdefddc10781bce4
Flamingfury56 <nwe4309@g.rit.edu>
2018-10-28 14:01:08 -0400

    Added sound manager and tweaked volumes

---
+++ Assets/Scripts/RainController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour
+{
+
+    public AudioClip lightRain;
+    public AudioClip mediumRain;
+    public AudioClip heavyRain;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject rain;
+    public GameObject thunder;
+    public GameObject doorMan;
+    AudioSource doorManAudio;
+
+	// Use this for initialization
+	void Start ()
+    {
+        doorManAudio = doorMan.GetComponent<AudioSource>();
+
+        player.GetComponent<AudioSource>().volume = 0.125f;
+        rain.GetComponent<AudioSource>().volume = 0.25f;
+        thunder.GetComponent<AudioSource>().volume = 0.25f;
+        doorManAudio.volume = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
